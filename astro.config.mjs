e9c84782828db119353d70c9265b3c2d28fb4c5c
Erick <erickalwd@Ericks-MacBook-Air.local>
2023-12-07 14:54:06 +0700

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fava-landing-page.netlify.app/",
-  integrations: [image({
-    serviceEntryPoint: 'astrojs/image/sharp'
-  }),sitemap]
+  site: "https://astro-moon-landing.netlify.app/",
+  integrations: [tailwind()],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
 });
