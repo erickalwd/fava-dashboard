c8d0ab61cc42edc19478b86512bf5c45c835934e
Erick <erickalwd@Ericks-MacBook-Air.local>
2023-12-07 13:22:16 +0700

    Add astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fava-landing-page.netlify.app/",
-  integrations: [tailwind()],
-  vite: {
-    ssr: {
-      external: ["svgo"],
-    },
-  },
+  integrations: [image({
+    serviceEntryPoint: 'astrojs/image/sharp'
+  }),sitemap]
 });
